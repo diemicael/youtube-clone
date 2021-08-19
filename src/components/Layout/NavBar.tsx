c920f2c225a3f9fc75d1fabfdc854ce9c6bb35a1
diemicael <dieison.micael@gmail.com>
2021-08-18 21:16:56 -0300

    Home Page

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -22,6 +22,7 @@ import Whatshot from '@material-ui/icons/Whatshot';
 import VideoLibrary from '@material-ui/icons/VideoLibrary';
 import History from '@material-ui/icons/History';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import { AddCircle } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   mobileDrawer: {
@@ -45,6 +46,9 @@ const useStyles = makeStyles((theme) => ({
   },
   listItemText: {
     fontSize: 14
+  },
+  subheader: {
+    paddingLeft: theme.spacing(3)
   }
 }));
 
@@ -62,6 +66,16 @@ const primaryMenu = [
 const secondaryMenu = [
   { id: 1, label: 'Biblioteca', icon: VideoLibrary },
   { id: 2, label: 'Histórico', icon: History }
+];
+
+const tertiaryMenu = [
+  { id: 1, label: 'Música', icon: AddCircle },
+  { id: 2, label: 'Esportes', icon: AddCircle },
+  { id: 3, label: 'Jogos', icon: AddCircle },
+  { id: 4, label: 'Filmes', icon: AddCircle },
+  { id: 5, label: 'Notícias', icon: AddCircle },
+  { id: 6, label: 'Ao vivo', icon: AddCircle },
+  { id: 7, label: 'Aprender', icon: AddCircle }
 ];
 
 export function NavBar() {
@@ -97,9 +111,7 @@ export function NavBar() {
           );
         })}
       </List>
-
       <Divider />
-
       <List>
         {secondaryMenu.map((item) => {
           const Icon = item.icon;
@@ -123,9 +135,7 @@ export function NavBar() {
           );
         })}
       </List>
-
       <Divider />
-
       <Box mx={4} my={2}>
         <Typography variant="body2">
           Faça login para curtir vídeos, comentar e se inscrever.
@@ -140,6 +150,36 @@ export function NavBar() {
           </Button>
         </Box>
       </Box>
+      <Divider />
+      <List
+        subheader={
+          <ListSubheader className={classes.subheader}>
+            O MELHOR DO YOUTUBE
+          </ListSubheader>
+        }
+      >
+        {tertiaryMenu.map((item) => {
+          const Icon = item.icon;
+          return (
+            <ListItem
+              key={item.id}
+              button
+              classes={{ root: classes.listItem }}
+              selected={isSelected(item)}
+            >
+              <ListItemIcon>
+                <Icon style={{ color: isSelected(item) && '#f44336' }} />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText
+                }}
+                primary={item.label}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
     </Box>
   );
 
